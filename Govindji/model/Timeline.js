41316c9a8c2ae2c84202485aacc60aba66d3d4ce
Ankur Mandil <ankurmandil@gmail.com>
2024-12-27 18:43:34 +0530

    complete_task

--- Govindji/model/Timeline.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const timelineSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  date: { type: Date, required: true, default:Date.now() },
-});
-
-module.exports = mongoose.model('Timeline', timelineSchema);
--- Govindji/routes/TimelineRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getTimelines, createTimeline } = require('../controller/TimelineController');
-const router = express.Router();
-
-router.post('/', createTimeline); 
-router.get('/', getTimelines); 
-
-module.exports = router;
